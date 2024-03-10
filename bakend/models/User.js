8c94619896fd8b41eff2e6f7a3c9dbc7a8898a91
roushan-sheik <roushansheik@gmail.com>
2024-03-10 09:46:37 +0600

    for timestamps add second argument timestamp true

--- bakend/models/User.js
+++ bakend/models/User.js
@@ -1,16 +1,20 @@
 import { Schema } from "mongoose";
-const UserSchema = new Schema({
-  name: String,
-  email: String,
-  password: String,
-  role: {
-    type: String,
-    enum: ["user", "admin"],
-    default: "user",
+const UserSchema = new Schema(
+  {
+    name: String,
+    email: String,
+    password: String,
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    status: {
+      type: String,
+      enum: ["pending", "approved", "block", "declined"],
+      default: "pending",
+    },
+    // for timestamps add second argument {timestamps: true}
   },
-  status: {
-    type: String,
-    enum: ["pending", "approved", "block", "declined"],
-    default: "pending",
-  },
-});
+  { timestamps: true }
+);

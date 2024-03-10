c464bbade7f65ab19f336a599e5c94e58660944a
roushan-sheik <roushansheik@gmail.com>
2024-03-10 09:01:58 +0600

    Connection to the database and error ehandling

--- bakend/app.js
+++ bakend/app.js
@@ -10,19 +10,26 @@ app.get("/", (req, res) => {
     .status(200)
     .json({ name: "Arifa moni", age: 20, email: "arifa@gmail.com" });
 });
-// connection url import
+
+//NOTE - connection url import
 let connectionURL = process.env.MONGO_URI;
 connectionURL = connectionURL.replace("<username>", process.env.USER_NAME);
 connectionURL = connectionURL.replace("<password>", process.env.USER_PASSWORD);
 // Give a Database name and url query parameter
 connectionURL = `${connectionURL}/${process.env.DATABASE_NAME}?${process.env.DATABASE_URL_QUREY}`;
-console.log("connection URL: ", connectionURL);
+// console.log("connection URL: ", connectionURL);
 //NOTE - Connection to the database
-mongoose.connect(connectionURL, {}).then(() => {
-  console.log("Database connected");
-  // app listen
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Server is running on port:http://localhost:${PORT}`);
+mongoose
+  .connect(connectionURL, {})
+  .then(() => {
+    console.log("Database connected");
+    // app listen
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port:http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Connection faild");
+    console.log("message: ", error.message);
   });
-});
